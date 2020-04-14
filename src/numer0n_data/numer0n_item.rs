1d587ff9baa9d049d8e8eb174479480189cd9d60
Lenna_kun <lenna_kun@Lenna-kunnoMacBook-Pro.local>
2020-04-14 14:50:22 +0900

    minor fixes

--- src/numer0n_data/numer0n_item.rs
+++ src/numer0n_data/numer0n_item.rs
@@ -7,14 +7,14 @@ use crate::packed_decimal::PackedDecimal;
 #[derive(Copy, Clone)]
 pub struct Numer0nItem {
     pub packed_decimal: PackedDecimal,
-    pub call_bit: [BitTable; 4],
+    pub bit_array: [BitTable; 4],
     pub bit_table: BitTable,
 }
 impl Numer0nItem {
     pub fn from(num: i32) -> Self {
         Numer0nItem {
             packed_decimal: packed_decimal::i32_to_packed_decimal(num),
-            call_bit: (0..4).fold([0 as BitTable; 4], |mut acc, x| {
+            bit_array: (0..4).fold([0 as BitTable; 4], |mut acc, x| {
                 acc[3-x] = bit_table::from_one_digit(num / 10i32.pow(x as u32) % 10);
                 acc
             }),
@@ -28,7 +28,7 @@ impl Numer0nItem {
     }
     
     pub fn eat_bite(self, call: &Self) -> usize {
-        (0..4).fold(0, |eb, i| if (self.bit_table & call.call_bit[i]) > 0 { eb + 1 } else { eb }) 
+        (0..4).fold(0, |eb, i| if (self.bit_table & call.bit_array[i]) > 0 { eb + 1 } else { eb }) 
     }
 }
 impl std::cmp::PartialEq for Numer0nItem {
@@ -38,6 +38,6 @@ impl std::cmp::PartialEq for Numer0nItem {
 }
 impl std::fmt::Display for Numer0nItem {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}{}{}{}", self.call_bit[0].trailing_zeros(), self.call_bit[1].trailing_zeros(), self.call_bit[2].trailing_zeros(), self.call_bit[3].trailing_zeros())
+        write!(f, "{}{}{}{}", self.bit_array[0].trailing_zeros(), self.bit_array[1].trailing_zeros(), self.bit_array[2].trailing_zeros(), self.bit_array[3].trailing_zeros())
     }
 }
